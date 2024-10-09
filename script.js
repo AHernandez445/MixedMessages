722f32a75b6db697e5b5739fdecc1a3e21268c4f
AHernandez <alexflinthernandez@gmail.com>
2024-10-09 13:25:03 -0400

    Finish the MixedMEssages program

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hello World");
+
+const randomMessage = () =>{
+    const firstPart = ["You are", "you will", "something"];
+    const secondPart = ["going to", "have a", "are very", "terrible"];
+    const thirdPart = ["have a great day", "have a shitty day", "very strong"];
+
+    const randomOne = firstPart[Math.floor(Math.random() * 2)];
+    const randomTwo = secondPart[Math.floor(Math.random() * 2)];
+    const randomThree = thirdPart[Math.floor(Math.random() * 2)];
+
+    $("#output").text(`${randomOne} ${randomTwo} ${randomThree}`);
+}
